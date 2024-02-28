2a249f5f1ddca9bf1e03464f2a4f21a748698109
lcc1990 <10719686+lcc1990@user.noreply.gitee.com>
2024-02-28 14:25:57 +0800

    重新开始编写

--- demo/demo002/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var num int = 10
-	var Num int = 20
-	fmt.Printf("num: %v\nNum: %v",num,Num)
-}
--- demo/utils/utils.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-var Age int
-var Name string
-
-func init() {
-	Age = 20
-	Name = "Tom"
-}
